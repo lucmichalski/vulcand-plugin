cdb556c0cd542c6f2533ed88b08b6d1c3ae613d8
YunxiangHuang <hyxqshk@vip.qq.com>
2015-10-22 17:51:12 +0800

    临时修复XFF被覆盖的问题
    临时修复XFF被覆盖的问题

--- addheader/addheader.go
+++ addheader/addheader.go
@@ -51,6 +51,9 @@ func (ahm *AddHeaderMiddleware) NewHandler(next http.Handler) (http.Handler, err
 
 func (ahh *AddHeaderHandler) SetProxyHeader(r *http.Request) {
 	for k, v := range ahh.SetProxy {
+		if upkey := strings.ToUpper(k); upkey == "X-FORWARDED-FOR" {
+			r.Header.Set("REALIP", "AH_"+(r.Header.Get("REALIP")))
+		}
 		if strings.HasPrefix(v, HeaderFlag) {
 			tmp := r.Header.Get(strings.TrimPrefix(v, HeaderFlag))
 			r.Header.Set(k, tmp)
